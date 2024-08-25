d06246d2d8261c1503a25e39e5b8137b4f4263f5
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2024-08-25 13:12:46 +0530

    image_captioning.py
    implemented image captioning by using hugging face model api

--- image_captioning.py
+++ image_captioning.py
@@ -1,8 +1,58 @@
 import os
 import io
-import base64
+import IPython
+from IPython import display
+
+from PIL import Image
+import base64 
 from dotenv import load_dotenv
 import json
 import requests
 from transformers import pipeline
 import gradio as gr
+
+from dotenv import load_dotenv
+load_dotenv()
+
+hf_api_key = os.getenv('HF_API_KEY')
+hf_base_model_url = os.getenv('HF_API_ITT_BASE')
+
+def get_completion(inputs, parameters=None, ENDPOINT_URL=hf_base_model_url):
+    headers = {
+      "Authorization": f"Bearer {hf_api_key}",
+      "Content-Type": "application/json"
+    }
+    data = { "inputs": inputs }
+    if parameters is not None:
+        data.update({"parameters": parameters})
+    response = requests.request("POST",
+                                ENDPOINT_URL,
+                                headers=headers,
+                                data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
+
+
+
+
+import gradio as gr 
+
+def image_to_base64_str(pil_image):
+    byte_arr = io.BytesIO()
+    pil_image.save(byte_arr, format='PNG')
+    byte_arr = byte_arr.getvalue()
+    return str(base64.b64encode(byte_arr).decode('utf-8'))
+
+def captioner(image):
+    base64_image = image_to_base64_str(image)
+    result = get_completion(base64_image)
+    return result[0]['generated_text']
+
+gr.close_all()
+demo = gr.Interface(fn=captioner,
+                    inputs=[gr.Image(label="Upload image", type="pil")],
+                    outputs=[gr.Textbox(label="Caption")],
+                    title="Image Captioning with BLIP",
+                    description="Caption any image using the BLIP model",
+                    allow_flagging="never")
+
+demo.launch()
